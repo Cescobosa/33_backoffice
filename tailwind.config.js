91dbc123c73b1be2cab013ae304ddd95393fbbfb
Cescobosa <escobosa@33producciones.es>
2025-09-05 12:29:55 +0200

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#d42842",
+        section: "#008aa4"
+      }
+    }
+  },
+  plugins: []
+};
